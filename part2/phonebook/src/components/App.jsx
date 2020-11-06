8b1a631ed55842c836166607ef82ab4dc979476c
Nuss93 <nasreenrzk@gmail.com>
2020-11-07 00:36:21 +0800

    EXERCISE 2.8

--- part2/phonebook/src/components/App.jsx
+++ part2/phonebook/src/components/App.jsx
@@ -2,49 +2,61 @@ import React, { Children, useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '0123456789' }
   ]) 
   // form data controll
-  const [ newName, setNewName ] = useState('')
+  // const [ input, setNewInput ] = useState('')
+  const [ input, setNewInput ] = useState({ name: '', number: '' })
 
   const handleChange = (evt) => {
-    setNewName(evt.target.value)
+    let newInput = {
+      ...input,
+      [evt.target.name] : evt.target.value
+    }
+    
+    setNewInput(newInput)
   }
 
-  const submitName = (event) => {
+  const submitPerson = (event) => {
     event.preventDefault()
 
-    let CHECKER = persons.findIndex(a => a.name.toLowerCase() === newName.toLowerCase())
+    console.log(persons, input);
+
+    let CHECKER = persons.findIndex(a => a.name.toLowerCase() === input.name.toLowerCase())
 
     if(CHECKER !== -1){
-      alert(`${newName} is already added to phonebook`)
-      setNewName('')
+      alert(`${input} is already added to phonebook`)
+      setNewInput('')
       return
     }
 
     let newPersons = [...persons]
-    let newData = { name : newName }
+    let newData = input
     newPersons.push(newData)
 
     setPersons(newPersons)
-    setNewName('')
+    setNewInput({ name: '', number: '' })
   }
 
   return (
     <div>
       <h2 className="m-0">Phonebook</h2>
-      <form onSubmit={submitName} className="mb-4">
+      <form onSubmit={submitPerson} className="mb-4">
         <div>
-          name: <input name="name" onChange={handleChange} value={newName} />
+          Name: <input name="name" type="text" onChange={handleChange} value={input.name} />
         </div>
         <div>
-          <button type="submit">add</button>
+          Number: <input name="number" type="number" onChange={handleChange} value={input.number} />
+        </div>
+
+        <div>
+          <button type="submit">Add</button>
         </div>
       </form>
 
       <h2>Numbers</h2>
       <ul>
-        {persons.map((data,index) => <li key={index}>{data.name}</li>)}
+        {persons.map((data,index) => <li key={index}>{data.name} {data.number}</li>)}
       </ul>
     </div>
   )
