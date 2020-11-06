6a53b2999841ed8e6efc44f5e0d15900d344f505
Nuss93 <nasreenrzk@gmail.com>
2020-11-07 00:23:20 +0800

    EXERCISE 2.6

---
+++ part2/phonebook/src/components/App.jsx
@@ -0,0 +1,44 @@
+import React, { Children, useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  // form data controll
+  const [ newName, setNewName ] = useState('')
+
+  const handleChange = (evt) => {
+    setNewName(evt.target.value)
+  }
+
+  const submitName = (event) => {
+    event.preventDefault()
+    console.log('button clicked')
+    let newPersons = [...persons]
+    let newData = { name : newName }
+    newPersons.push(newData)
+
+    setPersons(newPersons)
+  }
+
+  return (
+    <div>
+      <h2 className="m-0">Phonebook</h2>
+      <form onSubmit={submitName} className="mb-4">
+        <div>
+          name: <input name="name" onChange={handleChange} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((data,index) => <li key={index}>{data.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
